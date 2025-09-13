aac6e9f11f4088c2bdabf5d817b602383a18d715
WW <a@b.c>
2025-09-12 22:16:37 -0400

    little linting

--- slackdm.py
+++ slackdm.py
@@ -15,20 +15,20 @@ SLACK_C = slack_sdk.WebClient(token=TOKEN)
 
 
 try:
-    chan = "@" + sys.argv[1] 
+    chan = "@" + sys.argv[1]
 except IndexError as e:
     chan = CHAN
     os.system("logger \"$0 error decoding channel for slacksend at `date`\"")
 
 try:
-    msg = sys.argv[2] 
+    msg = sys.argv[2]
 except IndexError as e:
     msg = ERROR_MSG
     os.system("logger \"$0 error decoding message for slacksend at `date`\"")
 
 try:
     response = SLACK_C.chat_postMessage(channel=chan, text=msg)
-    
+
     os.system("logger \"$0 sent slack msg ({}@{}) at `date`\"".format(chan,msg))
 except Exception as e:
     print(e)
